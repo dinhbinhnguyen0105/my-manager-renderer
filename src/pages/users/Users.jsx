90be2d9df780e6828ee6ba1c4c6d9f7809bf53f9
NDB <ndb@192.168.2.5>
2024-12-29 14:29:36 +0700

    from macbook

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -13,9 +13,8 @@ function Users() {
             return <ListOfUser />;
         case "create-new-user":
             return <CreateNewUser />;
-            break;
-        case "check-all-user":
-            break;
+        // case "check-all-user":
+        //     break;
     }
     return (<></>);
 }
@@ -114,50 +113,106 @@ function ListOfUser() {
 }
 
 function CreateNewUser() {
+    const [dateState, setDateState] = useState(() => {
+        const today = new Date();
+        const month = today.getMonth() + 1;
+        const day = today.getDate();
+        const year = today.getFullYear();
+
+        const formattedDate = `${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}-${year}`;
+
+        return formattedDate;
+    });
+    const [uidState, setUidState] = useState("");
+    const [usernameState, setUsernameState] = useState("");
+    const [passwordState, setPasswordState] = useState("");
+    const [emailState, setEmailState] = useState("");
+    const [phonenumberState, setPhonenumberState] = useState("")
+    const [twofaState, setTwofaState] = useState("");
+    const [savebtnState, setSavebtnState] = useState(true);
+
+    const saveActionHandler = () => {
+        if(window.electronAPIs) {
+            setSavebtnState(false);
+            window.electronAPIs.send("action", {
+                action: "create-new-user",
+                data: {
+                    date: dateState,
+                    uid: uidState,
+                    username: usernameState,
+                    password: passwordState,
+                    email: emailState,
+                    phonenumber: phonenumberState,
+                    twofa: twofaState,
+                },
+            });
+        } else {
+            setSavebtnState(false);
+
+        }
+    };
+
+    useEffect(() => {
+        if(window.electronAPIs) {
+            window.electronAPIs.on("action", (event, data) => {
+                if(data.action === "create-new-user") {
+                    if(data.result === true) setSavebtnState(true)
+                }
+            });
+        } else {
+            setTimeout(() => setSavebtnState(true), 3000);
+        }
+    }, [savebtnState]);
 
     return (
         <div className="create-new-user">
             <div className="create-user__item create-user__title">
                 <h1>Create new user</h1>
             </div>
+            <div className="create-user__item create-user__date">
+                <label className="create-user__label create-user__date-label">
+                    date
+                </label>
+                <input type="text" className="create-user__input create-user__date-input" value={dateState} disabled />
+            </div>
             <div className="create-user__item create-user__uid">
                 <label className="create-user__label create-user__uid-label">
                     uid
                 </label>
-                <input type="text" className="create-user__input create-user__uid-input" />
+                <input type="text" className="create-user__input create-user__uid-input" onChange={e => setUidState(e.target.value)} value={uidState} />
             </div>
             <div className="create-user__item create-user__username">
                 <label className="create-user__label create-user__username-label">
                     username
                 </label>
-                <input type="text" className="create-user__input create-user__username-input" />
+                <input type="text" className="create-user__input create-user__username-input" onChange={e => setUsernameState(e.target.value)} value={usernameState} />
             </div>
             <div className="create-user__item create-user__password">
                 <label className="create-user__label create-user__password-label">
                     password
                 </label>
-                <input type="text" className="create-user__input create-user__password-input" />
+                <input type="text" className="create-user__input create-user__password-input" onChange={e => setPasswordState(e.target.value)} value={passwordState} />
             </div>
             <div className="create-user__item create-user__email">
                 <label className="create-user__label create-user__email-label">
                     email
                 </label>
-                <input type="text" className="create-user__input create-user__email-input" />
+                <input type="text" className="create-user__input create-user__email-input" onChange={e => setEmailState(e.target.value)} value={emailState} />
             </div>
             <div className="create-user__item create-user__phonenumber">
                 <label className="create-user__label create-user__phonenumber-label">
                     phone number
                 </label>
-                <input type="text" className="create-user__input create-user__phonenumber-input" />
+                <input type="text" className="create-user__input create-user__phonenumber-input" onChange={e => setPhonenumberState(e.target.value)} value={phonenumberState} />
             </div>
             <div className="create-user__item create-user__twofa">
                 <label className="create-user__label create-user__twofa-label">
                     2fa
                 </label>
-                <input type="text" className="create-user__input create-user__twofa-input" />
+                <input type="text" className="create-user__input create-user__twofa-input" onChange={e => setTwofaState(e.target.value)} value={twofaState} />
             </div>
             <div className="create-user__item create-user__actions">
-                <button className="create-user__action create-user__action__save">Save</button>
+                <button className={`create-user__action create-user__action__save${savebtnState ? "" : " disabled"}`} onClick={savebtnState ? saveActionHandler : null}>{savebtnState ? "Save" : "Saving..."}</button>
             </div>
         </div>
     );
